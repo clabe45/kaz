48a9879d40d0687b4c2b178307853606529edb54
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-07-20 18:50:29 -0400

    support binary items

--- hold/item.py
+++ hold/item.py
@@ -1,4 +1,4 @@
-from os import mkdir
+from os import mkdir, remove
 from os.path import join, exists
 import json
 
@@ -8,10 +8,33 @@ def load():
     if not exists(hold_home):
         return {}
     with open(join(hold_home, 'items.json'), 'r') as f:
-        return json.load(f)
+        items = json.load(f)
+        for name, value in items.items():
+            if value is None:
+                # null value means it's a pointer to a binary file, load from blobs/{name}
+                with open(join(hold_home, 'blobs', name), 'rb') as blob:
+                    items[name] = blob.read()
+        return items
 
-def save(items):
+def save(items, original_items):
     if not exists(hold_home):
         mkdir(hold_home)
+
     with open(join(hold_home, 'items.json'), 'w') as f:
+        # write new item list
+        for name, value in items.items():
+            if type(value) is bytes:
+                blobs = join(hold_home, 'blobs')
+                if not exists(blobs):
+                    mkdir(blobs)
+                with open(join(blobs, name), 'w+b') as blob:
+                    blob.write(value)
+                items[name] = None
+        # check for deleted items
+        for name, value in original_items.items():
+            if not name in items:
+                # it was deleted, now delete the blob if the item was binary
+                if type(value) is bytes:
+                    remove(join(hold_home, 'blobs', name))
+
         json.dump(items, f)
--- hold/main.py
+++ hold/main.py
@@ -1,6 +1,9 @@
+import os.path
+
 import click
 
 from hold import item
+from hold.constants import hold_home
 
 @click.group()
 def cli():
@@ -12,10 +15,10 @@ def cli():
 def ls():
     """Show all stashed items"""
 
-    click.echo('\n'.join(item.load().keys()))
+    click.echo('\n'.join(['{}{}'.format(name, ' (binary)' if type(value) is bytes else '') for name, value in item.load().items()]))
 
 def autocomplete_name(ctx, args, incomplete):
-    return [i for i in item.load() if i.startswith(incomplete)]
+    return [name for name in item.load().keys() if name.startswith(incomplete)]
 
 @cli.command()
 @click.argument('name', autocompletion=autocomplete_name)
@@ -30,16 +33,21 @@ def get(name):
     click.echo(items[name])
 
 @cli.command()
-@click.argument('name', autocompletion=autocomplete_name)
+@click.option('--binary/--no-binary', '-b/-B', default=False, help='Accept binary data from stdin')
 @click.option('--edit/--no-edit', '-e/-E', default=False, help='Open the item in the default text editor')
+@click.argument('name', autocompletion=autocomplete_name)
 @click.argument('value', required=False)
-def set(name, edit, value):
+def set(name, binary, edit, value):
     """Store a value in an item"""
 
     items = item.load()
+    original_items = dict(items)
     old = items[name] if name in items else None
 
     if edit:
+        if binary:
+            click.echo('Cannot edit a binary file', err=True)
+            return
         if value is not None:
             click.echo('Value cannot be present while --editor is set.', err=True)
             return
@@ -47,11 +55,20 @@ def set(name, edit, value):
         # click.edit() returns None when no changes were made, so account for that
         value = edited.rstrip() if edited is not None else old
     else:
-        if value is None:
-            value = click.prompt('Value')
+        if value is not None:
+            if binary:
+                click.echo('Value cannot be present while --binary is set.', err=True)
+                return
+        else:
+            if binary:
+                click.echo('Value: ', nl=False)
+                stdin = click.get_binary_stream('stdin')
+                value = stdin.read()
+            else:
+                value = click.prompt('Value')
 
     items[name] = value
-    item.save(items)
+    item.save(items, original_items)
     print("{} '{}'".format('Added' if old is None else ('Updated' if value != old else 'No changes made to'), name))
 
 @cli.command()
@@ -60,20 +77,23 @@ def remove(name):
     """Remove an item"""
 
     items = item.load()
+    original_items = dict(items)
     if not name in items:
         click.echo('No such item: {}'.format(name), err=True)
         return
 
     del items[name]
-    item.save(items)
+    item.save(items, original_items)
     click.echo("Removed '{}'".format(name))
 
 @cli.command()
 def clear():
     """Remove all items"""
 
+    original_items = item.load()
+    count = len(original_items)
     count = len(item.load())
-    item.save({})
+    item.save({}, original_items)
     click.echo('Removed {} item{}'.format(count, '' if count == 1 else 's'))
 
 if __name__ == '__main__':
