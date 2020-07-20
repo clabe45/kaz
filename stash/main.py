3bf9d9d09efef00c45f6392eb099a47b6d00c994
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-07-20 14:42:05 -0400

    add `get` command

--- stash/main.py
+++ stash/main.py
@@ -14,5 +14,17 @@ def ls():
 
     click.echo('\n'.join(item.load().keys()))
 
+@cli.command()
+@click.argument('name')
+def get(name):
+    """Print the value of an item"""
+
+    items = item.load()
+    if not name in items:
+        click.echo('No such item: {}'.format(name))
+        return
+
+    click.echo(items[name])
+
 if __name__ == '__main__':
     cli()
