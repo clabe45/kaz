78c3acd2e4116c04de8c78ed379fb01ace28b14d
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-07-20 14:37:06 -0400

    add `list` command

--- stash/main.py
+++ stash/main.py
@@ -1,10 +1,18 @@
 import click
 
+import item
+
 @click.group()
 def cli():
     """Simple store for text items"""
 
     pass
+
+@cli.command(name='list')
+def ls():
+    """Show all stashed items"""
+
+    click.echo('\n'.join(item.load().keys()))
 
 if __name__ == '__main__':
     cli()
