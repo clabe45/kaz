bce55fb6db19d7e4d502ad0a93a9b2df733b0b9e
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-07-21 17:15:46 -0400

    add readme metadata for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+from os.path import abspath, dirname, join
+
 from setuptools import setup, find_packages
+
+this_directory = abspath(dirname(__file__))
+with open(join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='kaz',
-    version='0.3.0',
+    version='0.3.0.post0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['click'],
@@ -12,6 +18,8 @@ setup(
     ''',
     # extra metadata
     description='A simple storage cli',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='storage cli',
     url='https://github.com/clabe45/kaz',
     author='Caleb Sacks',
